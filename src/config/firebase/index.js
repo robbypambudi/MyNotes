719d7cbe6799ae6523159d11b490852eb3b6ed61
Robby Pambudi <34505233+robbypambudi@users.noreply.github.com>
2022-01-04 00:21:58 +0700

    Update Firebase Config

--- src/config/firebase/index.js
+++ src/config/firebase/index.js
@@ -7,15 +7,15 @@ import { getDatabase } from "firebase/database";
 // import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCPna_L52mNHQzZ6RNqy4oJSj0kUeDKg94",
-	authDomain: "mynotes-robbypambudi.firebaseapp.com",
-	projectId: "mynotes-robbypambudi",
+	apiKey: "XXXX"
+	authDomain: "XXXX",
+	projectId: "XXXX",
 	storageBucket: "mynotes-robbypambudi.appspot.com",
-	messagingSenderId: "745214285065",
-	appId: "1:745214285065:web:66dff135f0ac269b60efd8",
-	measurementId: "G-8WQW3WLS6C",
+	messagingSenderId: "XXXXX",
+	appId: "XXXX",
+	measurementId: "XXXXX",
 	databaseURL:
-		"https://mynotes-robbypambudi-default-rtdb.asia-southeast1.firebasedatabase.app",
+		"XXXXX",
 };
 
 // Initialize Firebase
